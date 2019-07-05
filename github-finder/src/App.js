a6439a1d99a8007a317b7840e0dd299b1f05bde5
arigatopix <teeruch.k@gmail.com>
2019-07-05 16:33:42 +0700

    Component State

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,12 +1,14 @@
-import React, { Component } from "react";
-import Navbar from "./components/layout/Navbar";
-import "./App.css";
+import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Navbar />
+        <UserItem />
       </div>
     );
   }
