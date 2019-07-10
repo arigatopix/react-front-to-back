38d8a31f7e06d824ca7d7783ddbe1bc27d04bfc9
arigatopix <teeruch.k@gmail.com>
2019-07-10 16:35:30 +0700

    init Context API

---
+++ github-finder/src/context/github/GithubState.js
@@ -0,0 +1,14 @@
+// เหมือนเป็น Action คอยส่งให้ reducer
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+  SEARCH_USERS,
+  GET_USER,
+  CLEAR_USER,
+  GET_REPOS,
+  SET_LOADING,
+  SET_ALERT,
+  REMOVE_ALERT
+} from '../types';
---
+++ github-finder/src/context/github/githubContext.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+const githubContext = createContext();
+// Context Object ท่อส่งข้อมูล
+
+export default githubContext;
